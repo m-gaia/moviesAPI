66eae96c08503ee8163da742dbd93c83f97c07bd
m-gaia <gaia.marianela@gmail.com>
2023-06-23 16:09:46 -0300

    Slider-css

--- src/components/InicioSlider.jsx
+++ src/components/InicioSlider.jsx
@@ -12,95 +12,64 @@ import "./InicioSlider.css";
 // import required modules
 import { Autoplay, Parallax, Pagination, Navigation } from "swiper";
 
+import {get} from "../utils/httpCliente.js"
+import { useState, useEffect } from "react"
+
 export const  InicioSlider = () => {
 
-  return (
-    <>
-      <Swiper
-        style={{
-          "--swiper-navigation-color": "#fff",
-          "--swiper-pagination-color": "#fff",
-        }}
-        speed={600}
-        parallax={true}
-        autoplay={{
-            delay: 2500,
-            disableOnInteraction: false,
-          }}
-        pagination={{
-          clickable: true,
-        }}
-        navigation={true}
-        modules={[Autoplay, Parallax, Pagination, Navigation]}
-        className="mySwiper"
-      >
-        <div
-          slot="container-start"
-          className="parallax-bg"
+  const [peliculasAct,setPeliculasAct] = useState([])
+
+  
+
+  useEffect(()=>{
+    get("/movie/now_playing").then((data)=>{
+        setPeliculasAct(data.results);
+    })
+    },[])
+
+    return (
+      <>
+        <Swiper
           style={{
-            "background-image":
-              "url(https://swiperjs.com/demos/images/nature-1.jpg)",
+            "--swiper-navigation-color": "#fff",
+            "--swiper-pagination-color": "#fff",
           }}
-          data-swiper-parallax="-23%"
-        ></div>
-        <SwiperSlide className="container-swiper">
-          <div className="title" data-swiper-parallax="-300">
-            Slide 1
-          </div>
-          <div className="subtitle" data-swiper-parallax="-200">
-            Subtitle
-          </div>
-          <div className="text" data-swiper-parallax="-100">
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
-              dictum mattis velit, sit amet faucibus felis iaculis nec. Nulla
-              laoreet justo vitae porttitor porttitor. Suspendisse in sem justo.
-              Integer laoreet magna nec elit suscipit, ac laoreet nibh euismod.
-              Aliquam hendrerit lorem at elit facilisis rutrum. Ut at
-              ullamcorper velit. Nulla ligula nisi, imperdiet ut lacinia nec,
-              tincidunt ut libero. Aenean feugiat non eros quis feugiat.
-            </p>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className="title" data-swiper-parallax="-300">
-            Slide 2
-          </div>
-          <div className="subtitle" data-swiper-parallax="-200">
-            Subtitle
-          </div>
-          <div className="text" data-swiper-parallax="-100">
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
-              dictum mattis velit, sit amet faucibus felis iaculis nec. Nulla
-              laoreet justo vitae porttitor porttitor. Suspendisse in sem justo.
-              Integer laoreet magna nec elit suscipit, ac laoreet nibh euismod.
-              Aliquam hendrerit lorem at elit facilisis rutrum. Ut at
-              ullamcorper velit. Nulla ligula nisi, imperdiet ut lacinia nec,
-              tincidunt ut libero. Aenean feugiat non eros quis feugiat.
-            </p>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className="title" data-swiper-parallax="-300">
-            Slide 3
-          </div>
-          <div className="subtitle" data-swiper-parallax="-200">
-            Subtitle
-          </div>
-          <div className="text" data-swiper-parallax="-100">
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
-              dictum mattis velit, sit amet faucibus felis iaculis nec. Nulla
-              laoreet justo vitae porttitor porttitor. Suspendisse in sem justo.
-              Integer laoreet magna nec elit suscipit, ac laoreet nibh euismod.
-              Aliquam hendrerit lorem at elit facilisis rutrum. Ut at
-              ullamcorper velit. Nulla ligula nisi, imperdiet ut lacinia nec,
-              tincidunt ut libero. Aenean feugiat non eros quis feugiat.
-            </p>
-          </div>
-        </SwiperSlide>
-      </Swiper>
-    </>
-  );
-}
+          speed={600}
+          parallax={true}
+          autoplay={{
+              delay: 2500,
+              disableOnInteraction: false,
+            }}
+          pagination={{
+            clickable: true,
+          }}
+          navigation={true}
+          modules={[Autoplay, Parallax, Pagination, Navigation]}
+          className="mySwiper"
+        >
+          {/* <div
+            slot="container-start"
+            className="parallax-bg"
+            style={{
+              "background-image":
+                "url(https://swiperjs.com/demos/images/nature-1.jpg)",
+            }}
+            data-swiper-parallax="-23%"
+          ></div> */}
+            {peliculasAct.map(peliculaAct => (
+              <SwiperSlide key={peliculaAct.id} className="container-swiper swiper-slide2" style={{ backgroundImage: `url(https://image.tmdb.org/t/p/w1280/${peliculaAct.backdrop_path})` }}>
+                <div className="title" data-swiper-parallax="-300">
+                  {peliculaAct.title}
+                </div>
+                <div className="subtitle" data-swiper-parallax="-200">
+                  {peliculaAct.subtitle}
+                </div>
+                <div className="text" data-swiper-parallax="-100">
+                  <p>{peliculaAct.overview.substring(0, 200)}...</p>
+                </div>
+              </SwiperSlide>
+            ))}
+        </Swiper>
+      </>
+    );
+  }
